60beee7b764bc6b064bf956179bb2a88949d5369
Tito Garrido Ogando <titogarrido@gmail.com>
2018-08-06 20:33:40 -0300

    Added composer
    Added composer

--- suse-prereq.sh
+++ suse-prereq.sh
@@ -30,6 +30,31 @@ sudo easy_install pip
 
 sudo pip install docker-compose==1.13
 
+# Installing Golang
+cd /tmp
+wget --no-check-certificate https://storage.googleapis.com/golang/go1.9.3.linux-s390x.tar.gz
+tar -xvf go1.9.3.linux-s390x.tar.gz
+sudo mv -iv go /opt
+cd ~/
+cp -ipv .bashrc .bashrc_orig
+echo export GOPATH=/home/$USER/git >> .bashrc
+echo export GOROOT=/opt/go >> .bashrc
+echo export PATH=/opt/go/bin:/home/$USER/bin:\$PATH >> .bashrc
+
+# Installing composer
+npm install -g composer-cli@0.19
+npm install -g composer-rest-server@0.19
+npm install -g generator-hyperledger-composer@0.19
+npm install -g yo
+npm install -g composer-playground@0.19
+
+
+# Fetch some utils
+cd ~/
+git clone https://github.com/hyperledger/composer-sample-applications.git
+git clone https://github.com/hyperledger/composer-tools.git
+
+
 # Print installation details for user
 echo ''
 echo 'Installation completed, versions installed are:'
@@ -44,3 +69,5 @@ echo -n 'Docker Compose: '
 docker-compose --version
 echo -n 'Python:         '
 python -V
+echo -n 'Hyperledger Composer:         '
+composer --version
