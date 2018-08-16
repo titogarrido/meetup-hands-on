68aa94f4697917e2d947bb004b7046a268f51bc7
Tito Garrido Ogando <titogarrido@gmail.com>
2018-08-16 17:56:03 -0300

    Update install-fabric.sh

--- install-fabric.sh
+++ install-fabric.sh
@@ -5,3 +5,5 @@ export FABRIC_VERSION=hlfv11
 ./downloadFabric.sh
 ./startFabric.sh
 ./createPeerAdminCard.sh
+mkdir ~/playground
+nohup composer-playground >~/playground/playground.stdout 2>~/playground/playground.stderr & disown
